ca3319e8478c33bcec3132adbfa0e7f2e6f1559e
Carina <1810640048@fh-burgenland.at>
2021-03-26 23:04:31 +0100

    Test das gitignore zu verbessern

--- Gruppe3/RSA.cs
+++ Gruppe3/RSA.cs
@@ -2,11 +2,28 @@ using System;
 
 namespace Gruppe3
 {
+    /*
+        https://www.educative.io/edpresso/what-is-the-rsa-algorithm 
+    */
     public class RSA
     {
         public void print()
         {
             Console.WriteLine("1) asymmetrische Verschlüsselung (RSA)");          
+        }
+
+        public void keyGen(){
+            // evtl. schon von der anderen Aufgabe verwenden? 
+        }
+
+        public void encrypt()
+        {
+
+        }
+
+        public void decrypt()
+        {
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Crypto
-Aufgabe1
+Das hier sind unsere Todos! 
+Geplant ist es dann nur den Gruppe 3 Odrner abzugeben. 
+
+Wenn alles richtig aufgesetzt ist könnte ihr im Terminal mit dotnet run starten :) 
+
+Bitte immer während dem Programmieren kommentieren und nicht erst nachher. So is es dann leichter sinnvolle Doku zu betreiben. 
