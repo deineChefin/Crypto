5451065fea3168a29ce0b5951a910876d6ce32a6
Carina <1810640048@fh-burgenland.at>
2021-03-27 17:02:06 +0100

    Hinzufügen von Links für RSA

--- Gruppe3/RSA.cs
+++ Gruppe3/RSA.cs
@@ -3,8 +3,27 @@ using System;
 namespace Gruppe3
 {
     /*
+    Links: 
+
+        Pseudocode.. 
         https://www.educative.io/edpresso/what-is-the-rsa-algorithm 
+        https://eli.thegreenplace.net/2019/rsa-theory-and-implementation/ 
+        https://www.geeksforgeeks.org/rsa-algorithm-cryptography/
+        http://koclab.cs.ucsb.edu/teaching/cren/project/2018/Adamczyk+Magnussen.pdf 
+        http://www.crypto-uni.lu/jscoron/cours/mics3crypto/m3.pdf
+        https://scialert.net/fulltext/?doi=jas.2006.482.510 
+        https://de.wikipedia.org/wiki/RSA-Kryptosystem 
+
+        Stackoverflow.. 
+        https://stackoverflow.com/questions/7809490/rsa-elgamal-pseudocode
+
+        Für Primzahlen.. 
+        https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test 
+        
+        Für Padding.. 
+        https://en.wikipedia.org/wiki/Optimal_asymmetric_encryption_padding 
     */
+
     public class RSA
     {
         public void print()
@@ -13,7 +32,13 @@ namespace Gruppe3
         }
 
         public void keyGen(){
-            // evtl. schon von der anderen Aufgabe verwenden? 
+            // schon von der anderen Aufgabe verwenden 
+            KeyGen keygenerator = new KeyGen();
+        }
+
+        public void hashen(string input){
+            // schon von der anderen Aufgabe verwenden 
+            MD5 md5 = new MD5();
         }
 
         public void encrypt()
