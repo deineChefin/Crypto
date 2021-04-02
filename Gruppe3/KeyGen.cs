d02835c017e76f9a4f8cf7a4f4fd4a77fbd96af3
Teresa Sutrich <teresa.sutrich@wagner-sicherheit.at>
2021-04-02 18:08:27 +0200

    *

--- Gruppe3/KeyGen.cs
+++ Gruppe3/KeyGen.cs
@@ -1,6 +1,10 @@
 using System;
 namespace Gruppe3
 {
+
+    //https://www.codeproject.com/Articles/264077/Keygen-for-application
+    //https://www.c-sharpcorner.com/forums/how-to-generate-unique-producat-key-for-every-pc-for-c-sharp
+
     public class KeyGen
     {
          public void print()
