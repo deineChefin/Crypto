ed8a6321f77e19c5dac0749a387dc4d1dbd7a493
Carina Aschauer <carina.aschauer@outlook.com>
2021-05-02 15:59:57 +0200

    correct extgcd

--- Gruppe3/RSA.cs
+++ Gruppe3/RSA.cs
@@ -1,28 +1,24 @@
 using System.IO;
 using System;
+using System.Numerics;
 namespace Gruppe3
 {
     public class RSA
     {
         private Random random = new Random();
-        // Welches Files 
-        // private FileStream fileStream = File.Open("C:\\Users\\carin\\OneDrive\\Desktop\\test.txt", FileMode.OpenOrCreate);
-        // ent- oder verschlüsseln 
-        public void print()
-        {
-            Console.WriteLine("1) asymmetrische Verschlüsselung (RSA)");
-            RSAKey key = this.genarateRSAKey();
-            key.print();
+
+        public RSAKey Key {get;}
+
+        public RSA() {
+            this.Key = this.genarateRSAKey();
         }
 
-        public void parGen()
+        public void print()
         {
-            // für zyklische GRuppe wichtig 
-            // einigen auf parametern 
-
-            // man braucht d und (e,N)
-            // p und q brauchen wir nicht mehr --> muss man wegschmeißen 
-            // so kennt man die Faktorisierung nicht mehr erechnen /erkennen 
+            Console.WriteLine(" RSA");
+            this.Key.print();
+            this.convertFileToChunks();
+            // ent- oder verschlüsseln 
         }
 
         public RSAKey genarateRSAKey()
@@ -34,13 +30,19 @@ namespace Gruppe3
             int phiN = (p-1)*(q-1);
             int e;
             do {
-                // generates a number between 2 and phiN - 1 
+                // generates a number e between 2 and phiN - 1 
                 e = random.Next(2, phiN);
             }
             while (gcd(e, phiN) > 1);
-        
-            // transform the formula e*d = 1 (mod phiN)
-            int d = (1 + phiN) / e;
+         
+            int k;
+            do {
+                // generates a number k between 2 and phiN - 1 
+                k = random.Next(2, phiN);
+            }
+            while (gcd(e, k) != 1);          
+
+            int d = this.modInverse(99, 78);
             
             return new RSAKey(e, d, N);
         }
@@ -58,25 +60,80 @@ namespace Gruppe3
             return i;
         }
         
-        public void hash(string input)
-        {
-            // schon von der anderen Aufgabe verwenden 
-            MD5 md5 = new MD5();
-            // Wo brauche ich das im RSA? 
+        /**
+            extended gcd
+            calculates the greatest common divisor g of two numbers a and b 
+            and additionally the coefficients u and v of a representation of g as an integer linear combination
+        */
+        public int[] extgcd(int a, int b) {
+            // values defined by the algorithm 
+            int u = 1; 
+            int v = 0; 
+            int s = 0; 
+            int t = 1;
+
+            while (b != 0) {
+                int q = a / b;
+                int tempB = a - q * b;
+                a = b;
+                b = tempB;
+                int tempS = u - q * s;
+                u = s;
+                s = tempS;
+                int tempT = v - q * t;
+                v = t;
+                t = tempT;
+                   
+            }
+
+            return new int[]{a, u, v};
         }
 
-        public void encrypt(string enc, string sender)
-        {
-            // c = m^e (mod N )
+        /**
+            modinverse calculates the multiplicative inverse element a-1 of an element a in a group of integers k* 
+        */
+        public int modInverse(int a, int k) {
+            // g, u, v 
+            int[] extgcd = this.extgcd(a, k);
+            return extgcd[1] % k; 
+        }
+        
+        public string getFile() {    
+            string path = "C:\\Users\\carin\\OneDrive\\Desktop\\test.txt";
+            return path;
         }
 
-        public void decrypt(string dec, string receiver)
-        {
-            // m = c^d (mod N)
+        public void convertChunksToFile() {
+
         }
 
-        // Für unterschiedliche Aufgaben, unterschiedliche Schlüssel 
-        // sonst würde man ein verschlüsselte Nachricht anstatt zu signieren, entschlüsseln! 
+        public void convertFileToChunks() {
+            byte[] allBytes = File.ReadAllBytes(this.getFile());
+            
+            foreach (var chunk in allBytes)
+            {
+                int encChunk = this.encrypt(chunk);
+                int decChunk = this.decrypt(encChunk);
+                System.Console.WriteLine("Chunk: {0} Enc: {1} Dec: {2}", chunk, encChunk, decChunk);
+            }
+        }
+
+        /**
+           c = m^e (mod N )
+        */
+        public int encrypt(int input)
+        {   
+            return (int)BigInteger.ModPow(input, this.Key.ePubKey, this.Key.NPubKey);
+        }
+
+        /**
+           m = c^d (mod N)
+        */
+        public int decrypt(int chiffre)
+        {
+            return (int)BigInteger.ModPow(chiffre, this.Key.privateKey, this.Key.NPubKey);
+        }
+
         public void sign()
         {
             // s = m^d (mod N)
